8c71a58453e2ad7dfd76ad80a3371e50936ed205
Arda Dönmez <116437968+Kashidex@users.noreply.github.com>
2026-04-03 00:17:16 +0300

    Add files via upload

---
+++ ex15reversewordorder.py
@@ -0,0 +1,19 @@
+# EXERCISE 15: REVERSE WORD ORDER
+
+# Write a program (using functions!) that asks the user for a long string containing multiple words.
+# Print back to the user the same string, except with the words in backwards order. For example, say I type the string:
+# My name is Michele
+# Then I would see the string:
+# Michele is name My
+# shown back to me.
+
+###
+
+def reverseWordOrder(s):
+    words = s.split(sep=" ")
+    newString = ""
+    for i in words[::-1]:
+        newString = newString + " " + i
+    return newString.lstrip(" ")
+
+print(reverseWordOrder(input("Give me a string to reverse the word order of: ")))
